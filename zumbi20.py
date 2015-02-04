8ee2a9d1777c9b07da593f795df9d42d32551fd8
Leonardo Barros <nickolback@gmail.com>
2015-02-04 18:30:26 -0200

    Commit Inicial do projeto

---
+++ zumbi20.py
@@ -0,0 +1,9 @@
+nome = input('Digite o seu nome: ')
+print (nome)
+
+#n1 = int(input('Digite o primeiro numero: '))
+#n2 = int(input('Digite o segundo numero: '))
+
+n1 = float(input('Digite o primeiro numero: '))
+n2 = float(input('Digite o segundo numero: '))
+print(n1 + n2)
