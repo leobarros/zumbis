f21ed376b23ce0f9dc1d7f30dfd2f977ad2156df
Leonardo Barros <nickolback@gmail.com>
2015-02-04 18:36:28 -0200

    Questão 1 da lista

---
+++ lista_1/questao01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#04-02-15
+
+#Faça um programa que peça dois números
+#inteiros e imprima a soma desses dois números
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+
+print('Soma dos números: ', num1 + num2)
