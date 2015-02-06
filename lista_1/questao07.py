4827c0e164cde0dea87d588a6a369a68469631bf
Leonardo Barros <nickolback@gmail.com>
2015-02-06 06:26:55 -0200

    Questão 7 da lista

---
+++ lista_1/questao07.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32
+"""
+
+celsius = float(input("Temperatura em cº: "))
+
+fahrenheut = ((9 * celsius)/5) + 32
+
+print("Temperatura em Fahrenheit é: ", fahrenheut)
