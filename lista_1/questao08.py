3f18e06b78c55abfb1b8eb1ddcfa61095234c92f
Leonardo Barros <nickolback@gmail.com>
2015-02-09 22:17:43 -0200

    Questão08 da lista

---
+++ lista_1/questao08.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Faça agora o contrário, de Fahrenheit para Celsius.
+
+fahrenheit = float(input("Temperatura em Fº: "))
+celsius = ((fahrenheit - 32) / 1.8000)
+
+#ºC = ((ºF - 32)/1.8000)
+
+print ("Temperatura em Celsius é: ", celsius)
